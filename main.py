2d964fe1faed12f2dfc7f5c7aade4f323066ec06
tibeggs <tibeggs@gmail.com>
2021-01-11 13:46:26 -0500

    fix everything to normal

--- main.py
+++ main.py
@@ -41,8 +41,6 @@ Subject: %s
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("running webscraper 2")
-    {=
-    #run_scrapper()
+    run_scrapper()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
