29877d6edcc992816be8f0737a25ad59a859fd4c
tibeggs <tibeggs@gmail.com>
2021-01-11 14:25:11 -0500

    fix .py only

--- css/css.py
+++ css/css.py
@@ -1,7 +1,8 @@
+"""docstring"""
 import tinycss
 f = open("style.css", "r")
 
 parser = tinycss.make_parser()
-t=parser.parse_stylesheet(f.read()).errors
-}
+t = parser.parse_stylesheet(f.read()).errors
+
 print(len(t))
