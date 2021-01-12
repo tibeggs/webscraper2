13e60efa839436ed2f8e74f36e855a96ffd9a9a3
tibeggs <tibeggs@gmail.com>
2021-01-12 08:30:21 -0500

    mess with html

--- html/html_file.py
+++ html/html_file.py
@@ -1,7 +1,6 @@
 """docstring"""
 from bs4 import BeautifulSoup
 f = open("index.html", "r")
-print(f.read())
-k = bool(BeautifulSoup(f.read(), 'html.parser').find())
+k = bool(BeautifulSoup(f.read(), 'lxml').find())
 
 print(k)
