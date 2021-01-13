468fec6570b9a293e31e9d1db98f2005ca5d107e
tibeggs <tibeggs@gmail.com>
2021-01-13 11:26:31 -0500

    delete test code update log

--- css/css.py
+++
@@ -1,8 +0,0 @@
-"""docstring"""
-import tinycss
-f = open("style.css", "r")
-
-parser = tinycss.make_parser()
-t = parser.parse_stylesheet(f.read()).errors
-
-print(len(t))
--- js/javascript.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-import sys
-filename = "java.js"
-try:
-    subprocess.check_call(['node', '--check', filename])
-except subprocess.CalledProcessError:
-    print(f"Found {filename} that that has syntactical errors.", file=sys.stderr)
