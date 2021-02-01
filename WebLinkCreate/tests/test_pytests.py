4e0e55abea0a19133a11fdf1ded62bc7c048f7c7
tibeggs <tibeggs@gmail.com>
2021-02-01 13:08:54 -0500

    update log and tests

--- WebLinkCreate/tests/test_pytests.py
+++ WebLinkCreate/tests/test_pytests.py
@@ -35,7 +35,7 @@ class TestUrls:
     @pytest.mark.server
     def test_send_email(self):
         gmail_user = 'pyecono@gmail.com'
-        gmail_password = 'Econo2020!'
+        gmail_password = open("password.secret", "r").read()
         sent_from = gmail_user
         to = ['tibeggs@gmail.com', 'tbeggs@econometricainc.com']
         subject = 'WebScrape Result for test:'
